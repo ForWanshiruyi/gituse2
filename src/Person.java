bc7eadd52a68ec41d6d6a361433911dbdf211dad
wanshiruyi <wanshiruyi.com>
2022-07-17 00:16:00 +0800

    hot-666

--- src/Person.java
+++ src/Person.java
@@ -6,5 +6,6 @@ public class Person {
     public static void main(String[] args) {
         System.out.println("222");
         System.out.println("hot-444");
+        System.out.println("hot-666");
     }
 }
